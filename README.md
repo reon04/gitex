bcb2f7e4712aa8e500fbe1fa516342603e075c9f
reon04 <larsmichels04@gmail.com>
2026-03-18 09:10:58 +0100

    setup package structure

---
+++ README.md
@@ -0,0 +1,25 @@
+# gitex
+
+This package adds _definetly useful_ new git subcommands to your system.
+
+## Installation
+```
+git clone https://github.com/reon04/gitex.git
+cd gitex
+pip install --user .
+```
+Notes:
+- choose pip or pip3 accordingly
+- when using a venv the option `--user` is not needed
+
+### Uninstallation
+```
+pip uninstall gitex
+```
+
+## Usage
+This package adds the following git subcommands:
+- git lol: prints lol in the shell
+
+## License
+This code is licensed under the [MIT License](LICENSE).
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import site, sys
+import pathlib
+
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="gitex",
+    version="0.0.1",
+    description="This package adds 'definetly useful' new git subcommands to your system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reon04/gitex",
+    author="reon04",
+    author_email="larsmichels04@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14'
+    ],
+    packages=["gitex"],
+    license="MIT",
+    python_requires=">=3.8, <4",
+    install_requires=[],
+    entry_points={
+        "console_scripts": ["git-lol=gitex:lol"]
+    }
+)
